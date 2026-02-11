2f1ed489b28c4ab56c4e71acc357ddba707f314c
gayathri-jv8 <mallidigayathri1@gmail.com>
2026-02-11 10:32:52 +0530

    train_model.py

--- train_model.py
+++ train_model.py
@@ -1,38 +1,49 @@
 import pandas as pd
-from sklearn.ensemble import IsolationForest
-from sklearn.preprocessing import LabelEncoder
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-data = {
-    "amount": [500, 1000, 2000, 3000, 1500, 2500, 4000, 5000, 1200, 1800],
-    "location": [
-        "Hyderabad", "Hyderabad", "Bangalore", "Bangalore",
-        "Chennai", "Chennai", "Hyderabad", "Mumbai",
-        "Mumbai", "Hyderabad"
-    ]
-}
+df = pd.read_csv("data/creditcard.csv")
 
-df = pd.DataFrame(data)
+le_location = LabelEncoder()
+le_purpose = LabelEncoder()
 
-# Encode location (text → numbers)
-location_encoder = LabelEncoder()
-df["location_encoded"] = location_encoder.fit_transform(df["location"])
+df["location"] = le_location.fit_transform(df["location"])
+df["purpose"] = le_purpose.fit_transform(df["purpose"])
 
-X = df[["amount", "location_encoded"]]
+df["date"] = pd.to_datetime(df["date"])
+df["day"] = df["date"].dt.day
+df["month"] = df["date"].dt.month
+df["hour"] = df["time"].apply(lambda x: int(x.split(":")[0]))
 
-# Train Isolation Forest
-model = IsolationForest(
-    n_estimators=200,
-    contamination=0.1,
+df.drop(["date", "time"], axis=1, inplace=True)
+
+X = df.drop("prediction", axis=1)
+y = df["prediction"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier(
+    n_estimators=100,
     random_state=42
 )
 
-model.fit(X)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+model_bundle = {
+    "model": model,
+    "location_encoder": le_location,
+    "purpose_encoder": le_purpose
+}
 
 with open("fraud_model.pkl", "wb") as f:
-    pickle.dump(model, f)
+    pickle.dump(model_bundle, f)
 
-with open("location_encoder.pkl", "wb") as f:
-    pickle.dump(location_encoder, f)
-
-print("✅ Fraud Detection Model trained with Amount + Location!")
+print("✅ Model + encoders saved ")
