8231b21728290a8c5ed60cf0cff03d0f5fcb9d34
gayathri-jv8 <mallidigayathri1@gmail.com>
2026-02-16 14:28:53 +0530

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect("customers.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    customer_id TEXT UNIQUE,
+    email TEXT UNIQUE,
+    phone TEXT,
+    password TEXT,
+    otp TEXT,
+    is_verified INTEGER DEFAULT 0
+);
+""")
+
+conn.commit()
+conn.close()
+
+print("✅ Database Created ")
